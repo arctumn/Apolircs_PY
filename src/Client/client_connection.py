72375fc0bd89e088416224e4e3259f90d22010aa
arctumn <arctumn@github.com>
2021-08-09 15:43:43 +0100

    Refactored, needs to be tested -> issues with circular imports

--- src/Client/client_connection.py
+++ src/Client/client_connection.py
@@ -56,11 +56,6 @@ def start() -> None:
             thread_receber.start()
             try:
                 while(True):
-                    
-                    #thread_receber = threading.Thread(target=receive_message, args=([ssock,nome]))
-                    #thread_receber_events = threading.Event()
-                    #thread.daemon = True
-                    #thread_receber.
                     send_message(ssock,nome)
     
             except Exception as e:
@@ -88,7 +83,7 @@ def send_message(soc:ssl.SSLSocket, nome:str):
         titpre = "Global Chat"
         AtualizaHistorico(calculaEspacos(122,cor_NOT + f" ### ESTÁ AGORA NO CHAT: { cor_TP + titpre + cor_NOT} ###" + cor_BRANCO))
         ImprimeMensagensGuardadas("Global Chat")
-        #mensagens_globais = getMessagesUSer(ListaMsgs,"Global Chat")
+       
 
     elif resposta == "2": # MENSAGENS PRIVADAS
         modo = 2
@@ -96,9 +91,7 @@ def send_message(soc:ssl.SSLSocket, nome:str):
         signature = make_key(nome,titpre)
         snd_msg = f"{nome}.|||.startGROUP{nome_utilizadores}{titpre}.__.{signature}"
         soc.send(snd_msg.encode())
-        #debug_admin("HERE")
-            #send_hand_shake_to_group(soc,nome, listausers, titpre)
-            #thread_receber.resume()
+
         AtualizaHistorico(calculaEspacos(122,cor_NOT + f" ### ESTÁ AGORA NO CHAT: { cor_TP + titpre + cor_NOT} ###" + cor_BRANCO))
         AddListaAmigos(calculaEspacos(20,titpre),0, False)
         ambienteGrafico()
@@ -106,7 +99,7 @@ def send_message(soc:ssl.SSLSocket, nome:str):
     elif resposta == "3":
         exit(0)
     else:
-        #print(cor_MENU + "Opção não reconhecida" + cor_BRANCO)
+        
         send_message(soc,nome)
 
     while(msg != "!exit"):
@@ -122,7 +115,7 @@ def send_message(soc:ssl.SSLSocket, nome:str):
             destino, _ = menuPM(nome)
             signature = make_key(nome,titpre)
             soc.send(f"{nome}.|||.startGROUP{destino}{titpre}.__.{signature}".encode('utf-8')) #ATENÇÃO, o for já add o .__. a seguir ao destino
-            #send_hand_shake_to_group(soc,nome, listausers, titpre)
+           
             AtualizaHistorico(calculaEspacos(122,cor_NOT + f" ### ESTÁ AGORA NO CHAT: { cor_TP + titpre + cor_NOT} ###" + cor_BRANCO))
             AddListaAmigos(calculaEspacos(20,titpre),0, False)
             ambienteGrafico()
@@ -130,41 +123,33 @@ def send_message(soc:ssl.SSLSocket, nome:str):
 
         elif (str(msg).casefold().find("!pm ") != -1):
             modo = 2
-            #debug_admin(msg)
             grupo = msg.removeprefix("!pm ")
-            #grupo = msg[4:]
-            #debug_admin(grupo)
+
             if grupo == "Global Chat":
                 modo = 1
                 titpre = "Global Chat"
             else:
                 titpre = grupo
-                #titpre = grupo + str(random.randint(0,9999))
-            #debug_admin(grupo)
+
             AddListaAmigos(calculaEspacos(20,titpre),0, False)
             AtualizaHistorico(calculaEspacos(122,cor_NOT + f" ### ESTÁ AGORA NO CHAT: { cor_TP + titpre + cor_NOT} ###" + cor_BRANCO))
             ImprimeMensagensGuardadas(titpre)
-            ambienteGrafico()
-            # if grupo não existe então:
-            #   destino = menuPM(nome)
-            #   soc.send(f"{nome}.|||.startGROUP{destino}{titpre}".encode('utf-8')) #ATENÇÃO, o for já add o .__. a seguir ao destino
-            #   AtualizaHistorico(calculaEspacos(122,f" ### ESTÁ AGORA NO CHAT: {titpre} ###"))
-            #   ambienteGrafico()
+
 
         elif modo == 1:
             ImprimeMensagensGuardadas("Global Chat")
             if messageParse(msg) != 1:
                 ambienteGrafico()
-                ##print("Mensagem: "+ msg)
+                
                 try:
                     if msg != "":
                         signature = sign(msg.encode(),nome)
                         new_msg = f"{nome}.|||.globalMSG{msg}.__.{signature}"
-                        #debug_admin(new_msg)
+                        
                         soc.sendall(new_msg.encode('utf-8'))
                 except Exception as e:
                     print(e.args)
-                    #debug_admin(msg)
+                    
 
         elif modo == 2:
             ImprimeMensagensGuardadas(titpre)
@@ -172,24 +157,24 @@ def send_message(soc:ssl.SSLSocket, nome:str):
                 try:
                     if msg != "":
                         sig = sign(msg.encode(),nome)
-                        #print(sig)
+                        
                         msg = encrypt_with_shared_key(titpre,msg)
                         soc.send(f"{nome}.|||.privateMSG{titpre}.__.{msg}.__.{sig}".encode('utf-8'))
                 except Exception as e:
                     print(e.args)
-                    #debug_admin(msg)
+                    
 
 
 
 def register(ssock:ssl.SSLSocket) -> Tuple:
-    #rand = random.randint(0,1000)
+    
     nome = input("Qual o seu nome? ")
     passwrd = input("Qual a sua password? ")
     salt, digested_password = gen_passwd(passwrd,"".encode(),True)
     ssock.send(f"register.__.{nome}.__.{digested_password}.__.{salt}".encode())
     ssock.recv(1024)
     allkeys= f'''{ssock.recv(3100).decode().removesuffix("'")}{ssock.recv(3100).decode().removesuffix("'")}{ssock.recv(3100).decode().removesuffix("'")}{ssock.recv(3100).decode().removesuffix("'")}'''
-    #print(allkeys)
+  
     try:
         save_keys(allkeys,f"{nome}")
     except Exception as e:
@@ -226,15 +211,15 @@ def login(ssock:ssl.SSLSocket) -> str:
             return ""
         nome = input("Qual o nome? ")
         password = input ("Qual a password? ")
-        #print("teste")
+        
         ssock.send(f"getsalt.__.{nome}".encode())
-        #print("teste")
+        
         salt = ssock.recv(1024)
-        #print(f"salt-> {salt}")
+       
         if salt:
             ssock.send(f"login.__.{nome}.__.{gen_passwd(password,salt, False)[1]}".encode('utf-8'))
             resp = ssock.recv(1024).decode()
-            #print(resp)
+            
             if resp == "Online":
                 return nome
         print("Erro no login tente novamente!")
